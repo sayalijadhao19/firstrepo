0be4b50e60af33e564ea7ae922b8bad10864263c
sayalijadhao19 <100017823+sayalijadhao19@users.noreply.github.com>
2023-03-19 00:45:05 +0530

    Add files via upload

---
+++ project in c atm.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <windows.h>
+#include <time.h>
+
+
+int main()
+{   system("color 3F");
+    int pin=1234,option,entered pin,count=0,amount=1;
+    float balance=5000;
+    int continuetransaction =1;
+
+
+    return 0;
+}
